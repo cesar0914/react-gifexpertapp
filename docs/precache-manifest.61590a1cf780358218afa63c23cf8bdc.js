7e3f6b9f57e22f716c5d7d9861cc1bdc4c2369cd
Cesar <cesar.vasquez0914@gmail.com>
2020-09-06 14:42:06 -0500

    docs agregado

---
+++ docs/precache-manifest.61590a1cf780358218afa63c23cf8bdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62beb80464cfdbc566609f6502fe3fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00dc335c6a769f9dd35f",
+    "url": "/static/css/main.0026e70b.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00dc335c6a769f9dd35f",
+    "url": "/static/js/main.b771eb7d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
